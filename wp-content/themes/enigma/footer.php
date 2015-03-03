f10f178c97a45bf9a2fd9c2327d3fa2cc0cf18c1
Elvis DAndrea <elvis.vista@gmail.com>
2015-03-02 22:18:25 -0300

    Rodape - estilo 3

--- wp-content/themes/enigma/footer.php
+++ wp-content/themes/enigma/footer.php
@@ -57,6 +57,7 @@
 				</div>
 				<div class="footer-column">
 					<ul>
+						<a href="http://refeitaria.com/">Início</a>
 						<li class="page_item page-item-7" data-original-title="" title=""><a href="http://refeitaria.com/?page_id=7">Contato</a></li>
 						<li class="page_item page-item-116" data-original-title="" title=""><a href="http://refeitaria.com/?page_id=116">Loja</a></li>
 					</ul>
